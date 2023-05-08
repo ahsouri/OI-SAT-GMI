5166d17a205b51850f77ce97fedf52795cacb2cb
Amir Souri <ahsouri@gmail.com>
2023-05-08 10:01:37 -0400

    the minus one was unnecessary

--- oisatgmi/averaging.py
+++ oisatgmi/averaging.py
@@ -4,7 +4,7 @@ from scipy.io import savemat
 
 
 def _daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days - 1)):
+    for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
 
