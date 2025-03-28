fixing the issue with error averaging

oisatgmi/averaging.py
@@ -8,6 +8,20 @@ def _daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
+def error_averager(error_X: np.array):
+    error_Y = np.zeros((np.shape(error_X)[1],np.shape(error_X)[2]))*np.nan
+    for i in range(0,np.shape(error_X)[1]):
+        for j in range(0,np.shape(error_X)[2]):
+            temp = []
+            for k in range(0,np.shape(error_X)[0]):
+                temp.append(error_X[k,i,j])
+            temp = np.array(temp)
+            temp[np.isinf(temp)]=np.nan
+            temp2 = temp[~np.isnan(temp)]
+            error_Y[i,j] = np.sum(temp2)/(np.size(temp2)**2)
+
+    error_Y = np.sqrt(error_Y)
+    return error_Y
 
 def averaging(startdate: str, enddate: str, reader_obj):
     '''
@@ -79,7 +93,7 @@ def averaging(startdate: str, enddate: str, reader_obj):
             sat_averaged_vcd[:, :, month - min(list_months), year - min(
                 list_years)] = np.squeeze(np.nanmean(sat_chosen_vcd, axis=0))
             sat_averaged_error[:, :, month - min(list_months), year - min(
-                list_years)] = np.sqrt(np.squeeze(np.nanmean(sat_chosen_error**2, axis=0)))
+                list_years)] = error_averager(sat_chosen_error**2)
             ctm_averaged_vcd[:, :, month - min(list_months), year - min(
                 list_years)] = np.squeeze(np.nanmean(ctm_chosen_vcd, axis=0))
         if np.size(sat_chosen_aux1) != 0:
@@ -99,6 +113,7 @@ def averaging(startdate: str, enddate: str, reader_obj):
             sat_averaged_vcd, axis=3).squeeze(), axis=2).squeeze()
         ctm_averaged_vcd = np.nanmean(np.nanmean(
             ctm_averaged_vcd, axis=3).squeeze(), axis=2).squeeze()
+        # TODO: we should update this but we never average over several months or years
         sat_averaged_error = np.sqrt(np.nanmean(np.nanmean(
             sat_averaged_error**2, axis=3).squeeze(), axis=2).squeeze())
         sat_aux1 = np.nanmean(np.nanmean(
@@ -108,6 +123,7 @@ def averaging(startdate: str, enddate: str, reader_obj):
     if sat_averaged_vcd.ndim == 3:
         sat_averaged_vcd = np.nanmean(sat_averaged_vcd, axis=2).squeeze()
         ctm_averaged_vcd = np.nanmean(ctm_averaged_vcd, axis=2).squeeze()
+        # TODO: we should update this but we never average over several months or years
         sat_averaged_error = np.sqrt(np.nanmean(
             sat_averaged_error**2, axis=2).squeeze())
         sat_aux1 = np.nanmean(sat_aux1, axis=2).squeeze()

oisatgmi/interpolator.py
@@ -35,8 +35,11 @@ def _boxfilter(size_kernel_x, size_kernel_y) -> np.array:
 
     return np.ones((int(size_kernel_x), int(size_kernel_y)))/(size_kernel_x*size_kernel_y)
 
+def _boxfilter2(size_kernel_x, size_kernel_y) -> np.array:
 
-def _upscaler(X: np.array, Y: np.array, Z: np.array, ctm_models_coordinate: dict, grid_size: float, threshold: float, tri=None):
+    return np.ones((int(size_kernel_x), int(size_kernel_y)))/(size_kernel_x*size_kernel_y)**2
+
+def _upscaler(X: np.array, Y: np.array, Z: np.array, ctm_models_coordinate: dict, grid_size: float, threshold: float, tri=None, error=False):
     '''
         upscaler function
         Input:
@@ -52,18 +55,18 @@ def _upscaler(X: np.array, Y: np.array, Z: np.array, ctm_models_coordinate: dict
     size_grid_model_lon = np.abs(ctm_longitude[0, 0]-ctm_longitude[0, 1])
     size_grid_model_lat = np.abs(ctm_latitude[0, 0] - ctm_latitude[1, 0])
 
-
-    if (size_grid_model_lon > grid_size) or (size_grid_model_lat > grid_size):
+    if (size_grid_model_lon >= grid_size) or (size_grid_model_lat >= grid_size):
         # upscaling is needed
         size_kernel_x = np.floor(size_grid_model_lon/grid_size)
         size_kernel_y = np.floor(size_grid_model_lat/grid_size)
-        if size_kernel_x == 0 : size_kernel_x = 1
-        if size_kernel_y == 0 : size_kernel_y = 1
-        kernel = _boxfilter(size_kernel_y, size_kernel_x)
-        # TO DO, in case of large jumps from small pixels to large ones
-        # you may exagerate the NaN boxes which seems to be a problem for
-        # satelite observations like GOSAT having too many gaps so I need 
-        # to figure out a better solution for this later
+        if size_kernel_x == 0:
+            size_kernel_x = 1
+        if size_kernel_y == 0:
+            size_kernel_y = 1
+        if error: # which kernel should we use?
+            kernel = _boxfilter2(size_kernel_y, size_kernel_x)
+        else:
+            kernel = _boxfilter(size_kernel_y, size_kernel_x)
         Z = signal.convolve2d(Z, kernel, boundary='symm', mode='same')
         # define the triangulation
         points = np.zeros((np.size(X), 2))
@@ -88,7 +91,7 @@ def _upscaler(X: np.array, Y: np.array, Z: np.array, ctm_models_coordinate: dict
         # upscaling is not needed but the model needs to match with sat
         upscaled_ctm_needed = True
         return X, Y, Z, upscaled_ctm_needed
-
+    
 
 def interpolator(interpolator_type: int, grid_size: float, sat_data, ctm_models_coordinate: dict, flag_thresh=0.75):
     '''
@@ -172,7 +175,7 @@ def interpolator(interpolator_type: int, grid_size: float, sat_data, ctm_models_
     print('....................... error')
     _, _, uncertainty, _ = _upscaler(lons_grid, lats_grid, _interpolosis(
         tri, sat_data.uncertainty**2*mask, lons_grid, lats_grid, interpolator_type, dists, grid_size),
-        ctm_models_coordinate, grid_size, threshold_ctm)
+        ctm_models_coordinate, grid_size, threshold_ctm, error=True)
     uncertainty = np.sqrt(uncertainty)
 
     # interpolate 3Ds fields for two-step retrievals (scattering weights, for example OMI NO2)
@@ -269,7 +272,6 @@ def interpolator(interpolator_type: int, grid_size: float, sat_data, ctm_models_
             _, _,  apriori_profile[z, :, :], _ = _upscaler(lons_grid, lats_grid,
                                                            _interpolosis(tri, sat_data.apriori_profile[z, :, :].squeeze()
                                                                          * mask, lons_grid, lats_grid, interpolator_type, dists, grid_size), ctm_models_coordinate, grid_size, threshold_ctm)
-    print(upscaled_ctm_needed)
     if isinstance(sat_data, satellite_opt):
         interpolated_sat = satellite_opt(vcd, sat_data.time, [], tropopause, latitude_center, longitude_center, [
         ], [], uncertainty, [], pressure_mid, averaging_kernels, upscaled_ctm_needed, [], [], [],

oisatgmi/pwv_cal.py
@@ -85,7 +85,7 @@ def pwv_calculator(ctm_data: list, sat_data: list):
             size_grid_model_lon = np.abs(ctm_longitude[0, 0]-ctm_longitude[0, 1])
             size_grid_model_lat = np.abs(ctm_latitude[0, 0] - ctm_latitude[1, 0])
             gridsize_ctm = np.sqrt(size_grid_model_lon**2 + size_grid_model_lat**2)
-            for z in range(0, np.shape(ctm_mid_pressure)[0]):
+            for z in range(0, np.shape(ctm_profile)[0]):
                 _, _, ctm_partial_column_new[z, :, :], _ = _upscaler(ctm_data[0].longitude, ctm_data[0].latitude,
                                                                      ctm_partial_column[z, :, :], sat_coordinate, gridsize_ctm, threshold_sat, tri=tri)
             ctm_partial_column = ctm_partial_column_new